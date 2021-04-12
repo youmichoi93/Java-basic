c8d6f86ef762062ae8e38ad611c21b330d9d2563
youmichoi93 <78770263+youmichoi93@users.noreply.github.com>
2021-04-12 15:43:55 +0900

    equals함수, 문자열의 주소

---
+++ CarExam.java
@@ -0,0 +1,22 @@
+public class CarExam {
+
+    
+    public static void main(String[] args){
+        String a = new String("Hello");
+        String b = new String("Hello");
+        String c = "Hello";
+        String d = "Hello";
+        
+       
+        System.out.println(b==c); //주소를 비교함 따라서 둘이 다름
+        System.out.println(c==d); //이 둘은 아예 같음 
+        System.out.println(a==b); //그러나 new로 만든 객체는 다름.
+        System.out.println(a.equals(b)); //a와 b를 글자만 비교하면 equals함수 둘의 글자는 같음.
+        System.out.println(System.identityHashCode(a));
+        System.out.println(System.identityHashCode(b));
+        System.out.println(System.identityHashCode(c));
+        System.out.println(System.identityHashCode(d));
+        
+    }
+    
+}
